88d423af92a537c146ff30a542955eca7d09f226
규영 이 <cloudning@gmail.com>
2026-01-22 20:40:07 +0900

    Chore: GitHub Actions CI/CD 테스트

--- src/main/java/geppetto/hellokubernetes/api/controller/HelloController.java
+++ src/main/java/geppetto/hellokubernetes/api/controller/HelloController.java
@@ -4,21 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        String podName = System.getenv("HOSTNAME");
-        if (podName == null || podName.isEmpty()) {
-            try {
-                podName = java.net.InetAddress.getLocalHost().getHostName();
-            } catch (java.net.UnknownHostException e) {
-                podName = "unknown";
-            }
-        }
-        log.info("Hello from Spring Boot on Hybrid K8s!!! (Running on " + podName + ")");
-        return "Hello from Spring Boot on Hybrid K8s!!! (Running on " + podName + ")";
+    public Map<String, String> hello() {
+        log.info("Hello Kubernetes! (CI/CD Success)");
+        return Map.of(
+                "message", "Hello Kubernetes! (CI/CD Success)",
+                "java_version", System.getProperty("java.version"),
+                "os_arch", System.getProperty("os.arch"),
+                "node_name", System.getenv().getOrDefault("NODE_NAME", "Unknown")
+        );
     }
 }
